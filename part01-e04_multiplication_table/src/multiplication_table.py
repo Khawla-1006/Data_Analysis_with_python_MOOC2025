acdbff2a1c41c71cf30b3d4f9eb74b5998f286f2
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-26 22:22:17 +0100

    multiplication_table

--- part01-e04_multiplication_table/src/multiplication_table.py
+++ part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,12 @@
 
 
 def main():
-    pass
+    for i in range(1,11):
+        for j in range(1,11):
+            print(f"{i*j:4d}", end="")
+            j += 1
+        print()
+        i+=1
 
 if __name__ == "__main__":
     main()
