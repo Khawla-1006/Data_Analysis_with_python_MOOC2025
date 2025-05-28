06c3254f7b4f127d7cc9d8317148fd7ce9bce5be
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-28 12:42:53 +0100

    triple_square

--- part01-e06_triple_square/src/triple_square.py
+++ part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+def triple(param):
+    return param*3
+
+def square(param):
+    return param**2
+
 
 def main():
-    pass
+    for i in range(1,10):
+        triple_i = triple(i)
+        square_i = square(i)
+        if square_i <= triple_i:
+            print(f"triple({i})=={triple_i} square({i})=={square_i}")
+        else:
+            break
+        
+
 
 if __name__ == "__main__":
     main()
