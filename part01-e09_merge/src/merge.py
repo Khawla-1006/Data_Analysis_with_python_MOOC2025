196a78a25e76051ce88142a1ccadde2832d73bd4
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-29 12:10:23 +0100

    merge_lists

--- part01-e09_merge/src/merge.py
+++ part01-e09_merge/src/merge.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    new_list = []
+    i = 0
+    j = 0
+    while i < len(L1) and j < len(L2):
+        if L1[i] <= L2[j]:
+            new_list.append(L1[i])
+            i += 1
+        else:
+            new_list.append(L2[j])
+            j += 1
+    new_list.extend(L1[i:])
+    new_list.extend(L2[j:])
+    return new_list
 
 def main():
-    pass
+    a = merge(sorted([1,2,3,4,5,2,4]),sorted([7,8,9,14]))
+    print(a)
 
 if __name__ == "__main__":
     main()
