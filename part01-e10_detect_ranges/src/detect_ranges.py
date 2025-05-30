a01527f9d25052cc8622c51aeaf2a84ea0849b97
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-30 21:44:22 +0100

    detect_ranges

--- part01-e10_detect_ranges/src/detect_ranges.py
+++ part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,7 +1,23 @@
-#!/usr/bin/env python3
-
 def detect_ranges(L):
-    return []
+    s = sorted(L)
+    my_list = []
+    i = 0
+    while i < len(s):
+        item = s[i]
+        if item+1 not in s:
+            my_list.append(item)
+        else:
+            start = item
+            end = item
+            s.remove(item)
+            while end+1 in L:
+                end = end+1
+                s.remove(end)
+            t = (start, end+1)
+            my_list.append(t)
+            i-=1
+        i+=1
+    return my_list
 
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
