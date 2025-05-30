ac55a2509c926e79bae82c5763105b4017213513
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-30 19:26:07 +0100

    solve quadratic

--- part01-e08_solve_quadratic/src/solve_quadratic.py
+++ part01-e08_solve_quadratic/src/solve_quadratic.py
@@ -3,11 +3,15 @@
 import math
 
 def solve_quadratic(a, b, c):
-    return (0,0)
+    r1 = (-b+math.sqrt((b**2 - 4*a*c)))/(2*a)
+    r2 = (-b-math.sqrt((b**2 - 4*a*c)))/(2*a)
+
+    return (r1,r2)
 
 
 def main():
-    pass
+    print(solve_quadratic(1,2,1))
+    # pass
 
 if __name__ == "__main__":
     main()
