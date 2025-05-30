081ada133304875e2dd8bfc56bd10aa01f881052
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-30 23:48:58 +0100

    distinct_chars

--- part01-e12_distinct_characters/src/distinct_characters.py
+++ part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    return {}
+    result = {}
+    for item in L :
+        result[item] = len(set(item))
+    return result
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
