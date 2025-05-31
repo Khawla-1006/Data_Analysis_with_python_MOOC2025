3122878852571cc5f0711e71c3e0b3c9f3e86c35
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-31 13:28:26 +0100

    revers_dict

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
-
+    result = {}
+    for old_key,old_value in d.items():
+        if len(old_value) == 1 :
+            already_there = result.get(old_value[0])
+            if already_there :
+                already_there.append(old_key)
+                result[old_value[0]] = already_there
+            else:
+                result[old_value[0]]= [old_key]
+        else:
+            for val in old_value:
+                result[val] = [old_key]
+    return result
 def main():
-    pass
+    #pass
+    d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+    print(reverse_dictionary(d))
+
+
 
 if __name__ == "__main__":
     main()
