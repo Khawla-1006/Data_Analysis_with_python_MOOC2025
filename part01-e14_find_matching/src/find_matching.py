d48d6143122918669122a111f8f39be462ad598b
khawla-1006 <khawla.ben.in@gmail.com>
2025-05-31 13:44:21 +0100

    find_matching.py

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    c = []
+    for i, word in enumerate(L):
+        if pattern in word:
+            c.append(i)
+
+    return c
 
 def main():
-    pass
+    result = find_matching(["sensitive", "engine", "rubbish", "comment"], "en")
+    print(result)
 
 if __name__ == "__main__":
     main()
