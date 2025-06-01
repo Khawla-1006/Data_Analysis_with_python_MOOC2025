b5c791a41068b3c3c70357091f23f47bab94a349
khawla-1006 <khawla.ben.in@gmail.com>
2025-06-01 20:57:04 +0100

    positive_number

--- part01-e17_positive_list/src/positive_list.py
+++ part01-e17_positive_list/src/positive_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
+    return list(filter(lambda x : x >0,L))
 
 def main():
-    pass
+    # pass
+    r = positive_list([2,-2,0,1,-7])
+    print(r)
 
 if __name__ == "__main__":
     main()
