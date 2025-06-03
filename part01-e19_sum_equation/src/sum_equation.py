ae949a58044bef6893c52cdbee41e7d34458f97a
khawla-1006 <khawla.ben.in@gmail.com>
2025-06-03 10:53:14 +0100

    sum_eq

--- part01-e19_sum_equation/src/sum_equation.py
+++ part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-
+from functools import reduce
 def sum_equation(L):
-    return ""
-
+    if not L:
+        return "0 = 0"
+    result = list(map(str,L))
+    return " + ".join(result) + f" = {sum(L)}"
 def main():
-    pass
+    print(sum_equation([1,2,3,7,5,8,9]))
 
 if __name__ == "__main__":
     main()
